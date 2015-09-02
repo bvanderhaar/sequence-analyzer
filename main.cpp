b93bb8c65827bd209471eb75b8a7f7a3b3dd1500
Brett VanderHaar <brett.vanderhaar@gmail.com>
2015-09-02 19:44:18 -0400

    reset author, add timing

--- main.cpp
+++ main.cpp
@@ -47,33 +47,50 @@ int main(int argc, char *argv[]) {
   clock_t begin_read = clock();
   string file_to_analyze = slurp(filename);
   clock_t end_read = clock();
-  double elapsed_secs_read = double(end - begin) / CLOCKS_PER_SEC;
+  double elapsed_secs_read = double(end_read - begin_read) / CLOCKS_PER_SEC;
 
   clock_t begin_gcount = clock();
   int gcount = count_string_occurance(file_to_analyze, 'G');
   clock_t end_gcount = clock();
-  double elapsed_secs_gcount = double(end_gcount - begin_ccount) / CLOCKS_PER_SEC;
+  double elapsed_secs_gcount =
+      double(end_gcount - begin_gcount) / CLOCKS_PER_SEC;
 
   clock_t begin_acount = clock();
   int acount = count_string_occurance(file_to_analyze, 'A');
-  clock_t end_read = clock();
-  double elapsed_secs_read = double(end - begin) / CLOCKS_PER_SEC;
+  clock_t end_acount = clock();
+  double elapsed_secs_acount =
+      double(end_acount - begin_acount) / CLOCKS_PER_SEC;
 
   clock_t begin_ccount = clock();
   int ccount = count_string_occurance(file_to_analyze, 'C');
-  clock_t end_read = clock();
-  double elapsed_secs_read = double(end - begin) / CLOCKS_PER_SEC;
+  clock_t end_ccount = clock();
+  double elapsed_secs_ccount =
+      double(end_ccount - begin_ccount) / CLOCKS_PER_SEC;
 
   clock_t begin_tcount = clock();
   int tcount = count_string_occurance(file_to_analyze, 'T');
-  clock_t end_read = clock();
-  double elapsed_secs_read = double(end - begin) / CLOCKS_PER_SEC;
+  clock_t end_tcount = clock();
+  double elapsed_secs_tcount =
+      double(end_tcount - begin_tcount) / CLOCKS_PER_SEC;
 
-  double total = (double) file_to_analyze.size();
+  double total = (double)file_to_analyze.size();
 
-  printf("Adenine (A) Count (Percentage): %i (%f%%) \n", acount, (acount/total)*100);
-  printf("Thymine (T) Count (Percentage): %i (%f%%) \n", tcount, (tcount/total)*100);
-  printf("Cytosine (C) Count (Percentage): %i (%f%%) \n", ccount, (ccount/total)*100);
-  printf("Guanine (G) Count (Percentage): %i (%f%%) \n", gcount, (gcount/total)*100);
+  printf("Adenine (A) Count (Percentage): %i (%f%%) \n", acount,
+         (acount / total) * 100);
+  printf("Thymine (T) Count (Percentage): %i (%f%%) \n", tcount,
+         (tcount / total) * 100);
+  printf("Cytosine (C) Count (Percentage): %i (%f%%) \n", ccount,
+         (ccount / total) * 100);
+  printf("Guanine (G) Count (Percentage): %i (%f%%) \n", gcount,
+         (gcount / total) * 100);
+
+  printf("Read Time: %f, "
+         "A Count Time: %f, "
+         "T Count Time: %f, "
+         "C Count Time: %f, "
+         "G Count Time: %f",
+         elapsed_secs_read, elapsed_secs_gcount, elapsed_secs_tcount,
+         elapsed_secs_ccount, elapsed_secs_gcount);
+
   return 0;
 }
